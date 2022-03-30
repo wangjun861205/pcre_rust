4b0de6a60cc6168d99182f4fd81af3683ccb7d11
wangjun <444055828@qq.com>
2022-03-30 23:04:52 +0800

    complete pcre invoking

--- src/main.rs
+++ src/main.rs
@@ -13,16 +13,30 @@ extern "C" {
         context: *const c_void,
     ) -> *const c_void;
     fn pcre2_get_error_message_8(errorcode: i32, buffer: *mut u8, bufflen: usize) -> i32;
+    fn pcre2_match_data_create_from_pattern_8(
+        code: *const c_void,
+        gcontext: *const c_void,
+    ) -> *mut c_void;
+    fn pcre2_match_8(
+        code: *const c_void,
+        subject: *const u8,
+        length: usize,
+        startoffset: usize,
+        options: u32,
+        match_data: *mut c_void,
+        mcontext: *const c_void,
+    ) -> i32;
+    fn pcre2_get_ovector_pointer_8(match_data: *const c_void) -> *const usize;
 }
 
 fn main() {
     unsafe {
         let mut error_code = 0;
         let mut error_offset = 0;
-        let pattern = ".*";
+        let pattern = r"(\w+), (\w+)";
         let re = pcre2_compile_8(
             pattern.as_ptr(),
-            10,
+            pattern.len(),
             0,
             &mut error_code,
             &mut error_offset,
@@ -34,6 +48,29 @@ fn main() {
             "re: {:?}, error code: {}, error_offset: {}",
             re, error_code, error_offset
         );
-        println!("error: {}", from_utf8(&buffer).unwrap())
+        println!("error: {}", from_utf8(&buffer).unwrap());
+        let match_data = pcre2_match_data_create_from_pattern_8(re, null());
+        let subject = "hello, world";
+        let rc = pcre2_match_8(
+            re,
+            subject.as_ptr(),
+            subject.len(),
+            0,
+            0,
+            match_data,
+            null(),
+        );
+        println!("rc: {}", rc);
+        let mut ovector = pcre2_get_ovector_pointer_8(match_data);
+        let v = ovector as *const [usize; 4];
+        println!("start: {}, end: {}", (*v)[0], (*v)[1]);
+        println!("start: {}, end: {}", (*v)[2], (*v)[3]);
+        for i in 0..rc {
+            println!("the {}th ovector: {:?}", i, ovector);
+            let v = ovector as *const [usize; 2];
+            println!("start: {}, end: {}", (*v)[0], (*v)[1]);
+            let new_address = ovector as u64 + 16;
+            ovector = new_address as *const usize;
+        }
     }
 }
